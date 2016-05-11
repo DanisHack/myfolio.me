0cf543a33e7219b5f0be4d6f1900173acb42576f
Danish Mohd <danishmohd3610@gmail.com>
2016-05-11 21:29:43 +0530

    basic template rendered

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('ui'));
+
 
 app.get('/', function(req, res){
 
-	res.send('Hello World');
+	//res.send('Hello World');
+	res.render('index.html')
 });
 
 app.listen(3000, function(){
